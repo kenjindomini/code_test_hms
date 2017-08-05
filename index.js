c48184234c869080cfee72c9f7e9fffa950648bb
Keith Olenchak <kenjin.domini@gmail.com>
2017-08-05 02:04:28 -0400

    hapi server skeleton

--- index.js
+++ index.js
@@ -1,3 +1,33 @@
 'use strict';
 
 const hapi = require('hapi');
+const participantController = require('./api/controllers/participant');
+
+const server = new hapi.Server();
+server.connection({
+  port: 3001,
+  host: 'localhost'
+});
+
+const routes = [
+  {
+    method: 'GET',
+    path: '/participant/{participant_id?}',
+    handler: participantController
+  },
+  {
+    method: 'POST',
+    path: '/participant',
+    handler: participantController
+  }
+];
+
+server.route(routes);
+
+server.start((err) => {
+  if(err) {
+    throw err;
+  }
+
+  console.log(`Server running at: ${server.info.uri}`);
+});
